8db0ef9da16f4d16697dce43a2d77f01a135e059
Julien Pecqueur (JPEC) <jpec@julienpecqueur.net>
2012-12-26 16:48:20 +0100

    Paquet DEBIAN V1.0 (corrections suite)

--- create_dpkg.sh
+++ create_dpkg.sh
@@ -2,6 +2,6 @@
 nano ./raspyplayer/DEBIAN/control
 cp RasPyPlayer.py ./raspyplayer/usr/local/bin/RasPyPlayer
 cp raspyplayer.png ./raspyplayer/usr/share/pixmaps/raspyplayer.png
-cp raspyplayer.desktop ./raspyplayer/usr/share/applications/raspyplayer.desktop
+cp RasPyPlayer.desktop ./raspyplayer/usr/share/applications/RasPyPlayer.desktop
 dpkg-deb -b raspyplayer
 echo "FIN !!!"
