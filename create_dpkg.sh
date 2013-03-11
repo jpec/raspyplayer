c73fc47d9aa7e0096806270a3f13c079791c2787
Julien Pecqueur (JPEC) <jpec@julienpecqueur.net>
2013-03-11 13:54:56 +0100

    Unification of the Raspberry Pi and the Linux version.

--- create_dpkg.sh
+++ create_dpkg.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+echo "Creating Debian's package..."
 nano ./raspyplayer/DEBIAN/control
 cp RasPyPlayer.py ./raspyplayer/usr/local/bin/raspyplayer
 cp raspyplayer.conf ./raspyplayer/etc/raspyplayer.conf
@@ -7,4 +8,14 @@ cp raspyplayer.desktop ./raspyplayer/usr/share/applications/raspyplayer.desktop
 chmod a+rw ./raspyplayer/etc/raspyplayer.conf
 chmod a+x ./raspyplayer/usr/local/bin/raspyplayer
 dpkg-deb -b raspyplayer
-echo "FIN !!!"
+echo "Creating Archlinux's package..."
+nano ./raspyplayer-aur/PKGBUILD
+mkdir ./raspyplayer-aur/raspyplayer-vx.x
+cp RasPyPlayer.py ./raspyplayer-aur/raspyplayer-vx.x/
+cp raspyplayer.conf ./raspyplayer-aur/raspyplayer-vx.x/
+cp raspyplayer.png ./raspyplayer-aur/raspyplayer-vx.x/
+cp raspyplayer.desktop ./raspyplayer-aur/raspyplayer-vx.x/
+chmod a+rw ./raspyplayer-aur/raspyplayer-vx.x/raspyplayer.conf
+chmod a+x ./raspyplayer-aur/raspyplayer-vx.x/RasPyPlayer.py
+cd ./raspyplayer-aur/
+echo "Please rename raspyplayer-vx.x and tar -cf raspyplayer-vx.x.tar.gz raspyplayer-vx.x and upload it on ftp."
