6bc2af3fde04041468aa5a701b3b29fe54067dfb
Julien Pecqueur (JPEC) <jpec@julienpecqueur.net>
2013-03-12 22:44:36 +0100

    Fix PKGBUILD

--- create_dpkg.sh
+++ create_dpkg.sh
@@ -8,14 +8,3 @@ cp raspyplayer.desktop ./raspyplayer/usr/share/applications/raspyplayer.desktop
 chmod a+rw ./raspyplayer/etc/raspyplayer.conf
 chmod a+x ./raspyplayer/usr/local/bin/raspyplayer
 dpkg-deb -b raspyplayer
-echo "Creating Archlinux's package..."
-nano ./raspyplayer-aur/PKGBUILD
-mkdir ./raspyplayer-aur/raspyplayer-vx.x
-cp RasPyPlayer.py ./raspyplayer-aur/raspyplayer-vx.x/
-cp raspyplayer.conf ./raspyplayer-aur/raspyplayer-vx.x/
-cp raspyplayer.png ./raspyplayer-aur/raspyplayer-vx.x/
-cp raspyplayer.desktop ./raspyplayer-aur/raspyplayer-vx.x/
-chmod a+rw ./raspyplayer-aur/raspyplayer-vx.x/raspyplayer.conf
-chmod a+x ./raspyplayer-aur/raspyplayer-vx.x/RasPyPlayer.py
-cd ./raspyplayer-aur/
-echo "Please rename raspyplayer-vx.x and tar -cf raspyplayer-vx.x.tar.gz raspyplayer-vx.x and upload it on ftp."
